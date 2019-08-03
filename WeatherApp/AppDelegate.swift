7e4a05d5adb4ac8f502943465a4e7aa949488c9f
KiwookJUNG <ismeeki17@naver.com>
2019-08-03 22:05:27 +0900

    오타 수정

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    var savePointList = [SavedPoint]() // 저장된 위치
+    var savedPointList = [SavedPoint]() // 저장된 위치
     var weatherDataList = [WeatherData]() // 날씨 정보
     
 
