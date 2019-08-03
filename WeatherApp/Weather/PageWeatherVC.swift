7cb88530b646f998483b1e327e29a2e8ce9182fd
KiwookJUNG <ismeeki17@naver.com>
2019-08-03 15:35:25 +0900

    GetWeatherInformation 프로토콜 구현 : 좌표 정보를 얻어와서 좌표 정보를 이용해 API를 호출하고 Data로 변환하여
    WeatherData로 초기화 시켜 AppDelegate에 위치한 공용저장소에 저장
    
    저장할 때는 weatherDataList 배열의 첫 번째 자리에 저장한다. 왜냐하면 콜렉션 뷰에서 가장 첫번째로 위치 (현재위치는 날씨앱에서 가장 첫번째에 보인다.)하기 때문이다.

--- WeatherApp/Weather/PageWeatherVC.swift
+++ WeatherApp/Weather/PageWeatherVC.swift
@@ -9,6 +9,10 @@
 import UIKit
 import MapKit
 
+protocol GetWeatherInformation {
+    func coordinateToWeather(longi : Double, lati : Double)
+}
+
 class PageWeatherVC: UIViewController {
     
     @IBOutlet var pageCollectionView: UICollectionView!
@@ -24,12 +28,51 @@ class PageWeatherVC: UIViewController {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
-        //locationManager.startUpdatingLocation()
         locationManager.requestLocation()
         
     }
     
+    
+//    // 뷰가 나타나기 직전 Location을 업데이트해준다.
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(false)
+//        locationManager.startUpdatingLocation()
+//    }
+//    // 뷰가 나타나고 난후 Location 업데이트를 중단한다.
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(false)
+//        locationManager.stopUpdatingLocation()
+//    }
+    
 }
+
+//MARK: - 좌표를 이용해 날씨 API를 통해 얻어온 정보로 변환하여 저장하는 프로토콜 구현
+extension PageWeatherVC: GetWeatherInformation {
+    func coordinateToWeather(longi: Double, lati: Double) {
+        // 이 좌표는 UserDefault에 저장될 필요가 없는 좌표이다.
+        // 왜냐하면 매번 갱신되기 떄문
+        
+        let frontURL = "http://api.openweathermap.org/data/2.5/"
+        let APPID = "&APPID=7557a2e7185bcdda5baa43838b942438"
+        
+        let todayURL = frontURL + "weather?lat=\(Int(lati))&lon=\(longi)" + APPID
+        let forecastURL = frontURL + "forecast?lat=\(Int(lati))&lon=\(longi)" + APPID
+        
+        let todayAPI: URL! = URL(string: todayURL)
+        let todayAPIData = try! Data(contentsOf: todayAPI)
+        
+        let forecastAPI: URL! = URL(string: forecastURL)
+        let forecastAPIData = try! Data(contentsOf: forecastAPI)
+        
+        let weatherInfo = WeatherData(todayData : todayAPIData, forecastData : forecastAPIData)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.weatherDataList.insert(weatherInfo, at: 0) // 0번째 자리에 추가.
+        // 왜냐하면 Collection View Cell의 첫번째 자리에 위치해야 하기 때문이다.
+    }
+}
+
+
 
 //MARK: - 콜렉션 뷰의 Extension 각 셀에 위치할 함수를 정해준다. 콜렉션 뷰는 총 2개이다.
 extension PageWeatherVC: UICollectionViewDataSource, UICollectionViewDelegate {
@@ -58,6 +101,7 @@ extension PageWeatherVC: UICollectionViewDataSource, UICollectionViewDelegate {
         
     }
 }
+
 
 
 //MARK: - 셀 사이즈와 셀과 셀사이의 사이즈를 정해주는 델리게이트 메소드
@@ -102,14 +146,17 @@ extension PageWeatherVC : UITableViewDataSource, UITableViewDelegate{
 //MARK: - Mapkit에 대한 Extension
 extension PageWeatherVC : CLLocationManagerDelegate {
     
+    // 좌표가 업데이트되면 호출되는 델리게이트 메소드
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
         
         let lastLocation: CLLocation = locations[locations.count - 1]
         
+        // 좌표가 업데이트 되면
+        coordinateToWeather(longi: lastLocation.coordinate.longitude, lati: lastLocation.coordinate.latitude)
+        
         print(String(format: "%.6f", lastLocation.coordinate.latitude))
         print(String(format: "%.6f", lastLocation.coordinate.longitude))
-        //convertToAddressWith(coordinate: lastLocation)
+        
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
