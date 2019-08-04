f24093329aedd3597f26400707e1c085f4b4c080
KiwookJUNG <ismeeki17@naver.com>
2019-08-04 15:46:33 +0900

    offset 제거

--- WeatherApp/Weather/GetWeatherInformation.swift
+++ WeatherApp/Weather/GetWeatherInformation.swift
@@ -13,7 +13,7 @@ import UIKit
 
 @objc protocol GetWeatherInformation {
     
-    func coordinateToWeather(longi: Double, lati: Double, current: Bool)
+    func coordinateToWeather(longi: Double, lati: Double)
     
     @objc optional func updateWeatherData()
     // viewDidLoad에서 UserDefault 저장소에 저장된 (세 번째 뷰 컨트롤러에서 저장할 예정인 데이터) [SavedPoint]를
@@ -26,7 +26,7 @@ import UIKit
 
 //MARK: - 좌표를 이용해 날씨 API를 통해 얻어온 정보로 변환하여 저장하는 프로토콜 구현
 extension PageWeatherVC: GetWeatherInformation {
-    func coordinateToWeather(longi: Double, lati: Double, current: Bool) {
+    func coordinateToWeather(longi: Double, lati: Double) {
         // 이 좌표는 UserDefault에 저장될 필요가 없는 좌표이다.
         // 왜냐하면 매번 갱신되기 떄문
         
@@ -45,13 +45,10 @@ extension PageWeatherVC: GetWeatherInformation {
         let weatherInfo = WeatherData(todayData : todayAPIData, forecastData : forecastAPIData)
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        if (current == true){
-            appDelegate.weatherDataList.insert(weatherInfo, at: 0) // 0번째 자리에 추가.
-            // 왜냐하면 Collection View Cell의 첫번째 자리에 위치해야 하기 때문이다.
-        } else {
-            // 현재 위치가 아니라면 그냥 배열에 추가해준다.
-            appDelegate.weatherDataList.append(weatherInfo)
-        }
-        
+    
+        appDelegate.weatherDataList.insert(weatherInfo, at: 0) // 0번째 자리에 추가.
+        // 왜냐하면 Collection View Cell의 첫번째 자리에 위치해야 하기 때문이다.
+    
     }
+    
 }
