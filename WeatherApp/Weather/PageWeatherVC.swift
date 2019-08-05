452f7b7495aa832a4f90b931a2f50137afd24eac
KiwookJUNG <ismeeki17@naver.com>
2019-08-05 14:13:41 +0900

    PageWeatherVC는 RooVC에서 받은 데이터를 View, Collection View, Table View 등 다양한 방법으로 데이터를 표핸해주는 View Controller

--- WeatherApp/Weather/PageWeatherVC.swift
+++ WeatherApp/Weather/PageWeatherVC.swift
@@ -7,32 +7,27 @@
 //
 
 import UIKit
-import MapKit
+
 
 class PageWeatherVC: UIViewController {
     
-    @IBOutlet var pageCollectionView: UICollectionView!
+    @IBOutlet var city: UILabel!
+    @IBOutlet var weather: UILabel!
+    @IBOutlet var temperature: UILabel!
     
-    let locationManager : CLLocationManager = CLLocationManager()
+    @IBOutlet var day: UILabel!
+    @IBOutlet var isToday: UILabel!
+    
+    @IBOutlet var maxTemperature: UILabel!
+    @IBOutlet var minTemperature: UILabel!
+    
+    var index : Int = 0
+    var weatherData : WeatherData?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 구현해야 할 코드 : UserDefault에서 좌표 정보를 읽어와 [weatherData]를 추가하는 메소드
-        
-        pageCollectionView.backgroundColor = .green
-        
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest // 정확도
-        locationManager.delegate = self
-        
-        //locationManager.requestWhenInUseAuthorization()
-        locationManager.requestLocation()
-        
-        locationManager.startMonitoringSignificantLocationChanges()
-        // 사용자의 위치가 500미터 이상 움직이면 locationManager(_:didUpdateLocations:) 메소드를 호출
-        
-    }
     
+    }
     
     
 }
@@ -41,122 +36,3 @@ class PageWeatherVC: UIViewController {
 
 
 
-//MARK: - 콜렉션 뷰의 Extension 각 셀에 위치할 함수를 정해준다. 콜렉션 뷰는 총 2개이다.
-extension PageWeatherVC: UICollectionViewDataSource, UICollectionViewDelegate {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == pageCollectionView {
-            return 6
-        } else {
-            return 10
-        }
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if collectionView == pageCollectionView {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pageCell", for: indexPath)
-            cell.backgroundColor = .red
-            
-            return cell
-        } else {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "hourCell", for: indexPath)
-            cell.backgroundColor = .yellow
-            
-            return cell
-        }
-        
-    }
-}
-
-
-
-//MARK: - 셀 사이즈와 셀과 셀사이의 사이즈를 정해주는 델리게이트 메소드
-extension PageWeatherVC : UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        if collectionView == pageCollectionView{
-            return CGSize(width: view.frame.width, height: view.frame.height)
-        } else {
-            return CGSize(width: 46, height: 71)
-        }
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        if collectionView == pageCollectionView {
-            return 0
-        } else {
-            return 10
-        }
-    }
-}
-
-
-//MARK: - 테이블 뷰 컨트롤러
-extension PageWeatherVC : UITableViewDataSource, UITableViewDelegate{
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 15
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "infoCell", for: indexPath)
-        
-        cell.backgroundColor = .blue
-        return cell
-    }
-}
-
-
-
-
-
-//MARK: - Mapkit에 대한 Extension
-extension PageWeatherVC : CLLocationManagerDelegate {
-    
-    // 좌표가 업데이트되면 호출되는 델리게이트 메소드
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        let lastLocation: CLLocation = locations[locations.count - 1]
-        
-        let doubleLati = lastLocation.coordinate.latitude as Double
-        let doubleLongi = lastLocation.coordinate.longitude as Double
-        
-        // 좌표가 업데이트 되면
-        coordinateToWeather(longi: doubleLongi, lati: doubleLati)
-        
-        print(String(format: "%.6f", lastLocation.coordinate.latitude))
-        print(String(format: "%.6f", lastLocation.coordinate.longitude))
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        let alert = UIAlertController(title: "에러", message: "사용자의 위치를 찾지못하였습니다.", preferredStyle: UIAlertController.Style.alert)
-        alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.cancel, handler: nil))
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        switch status {
-        case .authorizedAlways:
-            manager.requestWhenInUseAuthorization()
-        case .authorizedWhenInUse:
-            manager.requestWhenInUseAuthorization()
-        case .notDetermined:
-            manager.requestWhenInUseAuthorization()
-        case .restricted:
-            let alert = UIAlertController(title: "위치정보", message: "위치정보를 허용하지 않았으므로 현재 위치의 날씨는 나오지 않습니다. 설정에서 허용해주세요.", preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.cancel, handler: nil))
-            present(alert, animated: true, completion: nil)
-        case .denied:
-            let alert = UIAlertController(title: "위치정보", message: "위치정보를 허용하지 않았으므로 현재 위치의 날씨는 나오지 않습니다. 설정에서 허용해주세요.", preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.cancel, handler: nil))
-            present(alert, animated: true, completion: nil)
-        @unknown default:
-            let alert = UIAlertController(title: "위치정보", message: "알 수 없는 에러가 발생했습니다. 관리자에게 문의해주세요.", preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.cancel, handler: nil))
-            present(alert, animated: true, completion: nil)
-            
-        }
-    }
-    
-}
