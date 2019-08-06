13a8fdbd38bacc1deced7e4a4f5e0e313f043211
KiwookJUNG <ismeeki17@naver.com>
2019-08-06 22:58:36 +0900

    Collection View의 커스텀 셀

---
+++ WeatherApp/Cell/HourCell.swift
@@ -0,0 +1,17 @@
+//
+//  HourCell.swift
+//  WeatherApp
+//
+//  Created by 정기욱 on 06/08/2019.
+//  Copyright © 2019 kiwook. All rights reserved.
+//
+
+import UIKit
+
+class HourCell: UICollectionViewCell {
+    
+    @IBOutlet var time: UILabel!
+    @IBOutlet var weather: UILabel!
+    @IBOutlet var temperature: UILabel!
+    
+}
