847b79b7e1e4c87db1deb051332877cef9a805cd
KiwookJUNG <ismeeki17@naver.com>
2019-08-07 09:32:55 +0900

    Custom City Cell

---
+++ WeatherApp/Cell/CityCell.swift
@@ -0,0 +1,20 @@
+//
+//  CityCell.swift
+//  WeatherApp
+//
+//  Created by 정기욱 on 06/08/2019.
+//  Copyright © 2019 kiwook. All rights reserved.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+    
+    @IBOutlet var time: UILabel!
+    
+    @IBOutlet var temperature: UILabel!
+    
+    @IBOutlet var city: UILabel!
+    
+    
+}
