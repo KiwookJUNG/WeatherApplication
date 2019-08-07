9170523a8ad04dcf892e618e54ecec99ae0475c9
KiwookJUNG <ismeeki17@naver.com>
2019-08-07 09:33:13 +0900

    Custom Day Cell

---
+++ WeatherApp/Cell/DayCell.swift
@@ -0,0 +1,23 @@
+//
+//  DayCell.swift
+//  WeatherApp
+//
+//  Created by 정기욱 on 07/08/2019.
+//  Copyright © 2019 kiwook. All rights reserved.
+//
+
+import UIKit
+
+class DayCell: UITableViewCell {
+    
+    
+    @IBOutlet var day: UILabel!
+    
+    @IBOutlet var weather: UILabel!
+    
+    @IBOutlet var maxTemperature: UILabel!
+    
+    @IBOutlet var minTemperature: UILabel!
+    
+    
+}
