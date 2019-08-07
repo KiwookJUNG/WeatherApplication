b68d409b8a658d3248e2ab950cd19600df5b207e
KiwookJUNG <ismeeki17@naver.com>
2019-08-07 09:33:30 +0900

    Custom Info Cell

---
+++ WeatherApp/Cell/InfoCell.swift
@@ -0,0 +1,18 @@
+//
+//  InfoCell.swift
+//  WeatherApp
+//
+//  Created by 정기욱 on 07/08/2019.
+//  Copyright © 2019 kiwook. All rights reserved.
+//
+
+import UIKit
+
+class InfoCell: UITableViewCell {
+    
+    
+    @IBOutlet var firstCell: UILabel!
+    
+    @IBOutlet var secondCell: UILabel!
+    
+}
